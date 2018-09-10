611cf14a0ff6e79ae0c43511f3d0692ad3ea2de2
Zack Jackson <z.jackson58@gmail.com>
2018-09-09 20:26:42 -0400

    Search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,31 @@
-// Global app controller
 import Search from "./models/Search";
 
-const search = new Search("pizza");
-console.log(search);
-search.getResults();
+/** GLOBAL STATE OF THE APP
+ *  - select object
+ *  - current recipe object
+ *  - shopping list object
+ *  - liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+  // 1) Get query from the view
+  const query = "pizza"; // todo
+  if (query) {
+    // 2) New search object and add it to state
+    state.search = new Search(query);
+
+    // 3) Prepare the UI for the results
+
+    // 4) Search for recipes
+    await state.search.getResults();
+
+    // 5) Render results on UI
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", e => {
+  e.preventDefault();
+  controlSearch();
+});
